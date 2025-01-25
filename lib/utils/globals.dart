91eb6a3a219bb1e130faeb7771b2080882426533
hafedh049 <165791939+ibtihelsalhi@users.noreply.github.com>
2025-01-25 22:00:20 +0100

    C

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -11,4 +11,4 @@ final AssetsAudioPlayer recitator = AssetsAudioPlayer.newPlayer();
 
 Box? userData;
 
-final Map<String, String> arabicNumbers = <String, String>{"0": "٠", "1": "۱", "2": "۲", "3": "۳", "4": "٤", "5": "٥", "6": "٦", "7": "۷", "8": "۸", "9": "۹"};
+const Map<String, String> arabicNumbers = <String, String>{"0": "٠", "1": "۱", "2": "۲", "3": "۳", "4": "٤", "5": "٥", "6": "٦", "7": "۷", "8": "۸", "9": "۹"};
